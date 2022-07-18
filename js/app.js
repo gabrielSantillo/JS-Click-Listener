0e785e649c0473fbcfe2a4f560aab748821c1a62
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-18 15:57:12 -0600

    Addind comments describing what the functions are doing

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
+/*This function changes the color of the element to gray. By getting my h2 tag by its id and adding an Event Listener to when the mouse hover it, I call the function that will change the color of the h2 tag.*/
 function changeColor(details) {
     details[`target`][`style`][`color`] = `gray`
 }
 let header = document.getElementById(`header-h2`);
 header.addEventListener(`mouseover`, changeColor);
 
+/*This function changes the sixe and background color of the element. By getting the first p tag of the page and clicking on it, I call this function that will change the size and background color */
 function changeFontSize(details) {
     details[`target`][`style`][`backgroundColor`] = `green`
     details[`target`][`style`][`fontSize`] = `2rem`
@@ -11,10 +13,16 @@ function changeFontSize(details) {
 let p_tags = document.querySelector(`p`)
 p_tags.addEventListener(`click`, changeFontSize);
 
-/**keyup */
-
-function footerBigger(details) {
-    details[`target`][`style`][`fontSize`] = `4rem`
+/*This function changes the background color of the element. By getting the body tag when the user press the space key, Icall the function that will change the background color of the body */
+function changeColorKeyUp(details) {
+    details[`target`][`style`][`backgroundColor`] = `black`;
 }
 let footerTag = document.querySelector(`body`);
-footerTag.addEventListener(`keyup`, footerBigger);
+footerTag.addEventListener(`keyup`, changeColorKeyUp);
+
+/*This function changes the backgroud color of the body. By using the function setTimeOut, I call the function that will change the background color and after 15s will change the color of the body. */
+function changeBgColor() {
+    let target = document.body;
+    target.style.background = `red`;
+}
+setTimeout(changeBgColor, 15000);
